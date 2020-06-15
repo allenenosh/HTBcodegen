e8a41c3e1b5a33456479db72ed94418f0eb1ae0a
E4U~Allen <49747518+allenenosh@users.noreply.github.com>
2020-06-15 20:26:02 +0530

    Add files via upload

---
+++ codegen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# genrating the key
+
+#showoff of the author and copy the code with credit dont be a baby it is my first bash script so it is not so good i will improve it in future
+apt install figlet
+
+figlet hackthebox code gen 
+
+
+echo genrate the invite code ? press enter
+
+read varnamne
+
+echo  ok the  following  code will be genrated in base64 formate $varname
+
+
+ curl -XPOST https://www.hackthebox.eu/api/invite/generate
+
+
+
+
+
+         echo  to decode the following output vist  https://www.base64decode.org/
+
+
+figlet script ended  subscribe to NEON SEC youtube
+
+
+
+
+
+
+
+
